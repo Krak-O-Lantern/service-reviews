f46f4a31a5287b8440ff8ef787e053f8987425f1
dannyhannyford <dannyhannyford@gmail.com>
2020-10-21 21:21:04 -0700

    connects to cssandra and exports client to preform queries

--- database/index.js
+++ database/index.js
@@ -1,7 +1,15 @@
-const mongoose = require('mongoose');
+const cassandra = require('cassandra-driver');
 
-const db = mongoose.connect('mongodb://database/fec', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to mongoose'))
-  .catch((err) => console.log(err));
+const client = new cassandra.Client({ contactPoints: ['127.0.0.1'], localDataCenter: 'datacenter1' });
 
-module.exports = db;
+client.connect()
+  .then(() => {
+    console.log('Connected to cluster with %d host(s): %j', client.hosts.length, client.hosts.keys());
+    console.log('Keyspaces: %j', Object.keys(client.metadata.keyspaces));
+  })
+  .catch((err) => {
+    console.error('There was an error when connecting', err);
+    return client.shutdown().then(() => { throw err; });
+  });
+
+module.exports = client;
