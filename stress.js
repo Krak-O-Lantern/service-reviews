732c0e46b5ffe88957f366a391d8f640f45f45b6
dannyhannyford <dannyhannyford@gmail.com>
2020-10-24 14:00:37 -0700

    stress testing files

--- stress.js
+++ stress.js
@@ -2,8 +2,8 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-  vus: 100,
-  duration: '60s',
+  vus: 200,
+  duration: '120s',
 };
 export default function() {
   const id = Math.floor(Math.random() * 10000000) + 1;
